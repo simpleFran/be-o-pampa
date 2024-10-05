19ca173532d04e02c01190fb0f343d6b024e8249
simpleFran <frank.m.camargo@gmail.com>
2024-10-05 20:07:18 -0300

    updated Cards

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -3,29 +3,32 @@ import React from "react";
 
 const PodcastCard = ({
   imgURL: imgUrl,
+
   title,
 }: {
   imgURL: string;
-  podcastId: number;
-  description: string;
   title: string;
 }) => {
   return (
     <div className="cursor-pointer">
-      <figure className="flex flex-col gap-2">
+      <figure className="flex flex-col ">
         <Image
           src={imgUrl}
           width={128}
           height={128}
           alt={title}
-          className="object-fit rounded-full border-green-2 border-y-2"
+          // className="object-fit rounded-full border-green-2 border-y-2"
+          className="object-cover rounded-lg border-green-2 border-y-2 w-96 h-44"
         />
-        <div className="flex flex-col">
-          <h1 className="truncate font-extrabold feef-h1 text-[1.5rem] text-white-1">
+        <h1 className="truncate font-extrabold feef-h1 text-[1.5rem] text-white-1 mx-auto">
             {title}
           </h1>
+        {/* <div className="flex flex-col"> */}
+          {/* <h1 className="truncate font-extrabold feef-h1 text-[1.5rem] text-white-1 mx-auto">
+            {title}
+          </h1> */}
           {/* <h2 className="text-[1.2rem]  truncate font-bold capitalize text-white-1">{description}</h2> */}
-        </div>
+        {/* </div> */}
       </figure>
     </div>
   );
